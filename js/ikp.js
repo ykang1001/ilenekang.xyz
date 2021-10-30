15dc3f7cf2446b2f3b20bb86589102741f8a1eff
ykang1001 <y.kang@rochester.edu>
2021-10-30 18:18:34 -0400

    cleaning up js and css

--- js/ikp.js
+++ js/ikp.js
@@ -1,19 +1,42 @@
-$(window).on('scroll', function() {
-    $(".project-section").each(function() {
-      if (isScrolledIntoView($(this))) {
-        $('a[href$="' + this.id + '"]').addClass('current');
-      }else{
-        $('a[href$="' + this.id  + '"]').removeClass('current');
-      }
-    });
-  });
-  
-  function isScrolledIntoView(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-  
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-  
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+// 
+WebFontConfig = {
+  google: {
+    families: ["Homemade Apple:regular", "Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900"]
   }
+};
+
+(function(d) {
+  var wf = d.createElement('script'), s = d.scripts[0];
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+  wf.async = true;
+  s.parentNode.insertBefore(wf, s);
+   })(document);
+
+!(function (o, c) {
+  var n = c.documentElement,
+    t = " w-mod-";
+  (n.className += t + "js"),
+    ("ontouchstart" in o || (o.DocumentTouch && c instanceof DocumentTouch)) &&
+      (n.className += t + "touch");
+})(window, document);
+
+
+$(window).on("scroll", function () {
+  $(".p__section").each(function () {
+    if (isScrolledIntoView($(this))) {
+      $('a[href$="' + this.id + '"]').addClass("--current");
+    } else {
+      $('a[href$="' + this.id + '"]').removeClass("--current");
+    }
+  });
+});
+
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return elemBottom <= docViewBottom && elemTop >= docViewTop;
+}
